6ddedf1f6729889daa74163fd558640d6cec03b1
vishal1145 <vishal.kumar1145@gmail.com>
2025-02-15 19:19:35 +0530

    footer background change

--- algo/components/sections/FooterSection.tsx
+++ algo/components/sections/FooterSection.tsx
@@ -5,7 +5,7 @@ export function FooterSection() {
   return (
     <>
     
-    <footer className="bg-gray-900 text-white py-12">
+    <footer className="bg-white text-white py-12">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-5 gap-12">
           {/* Logo and Description */}
